45dbd4c97cef679b93c5015b55d96ba1ac29bfd8
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-23 22:43:30 +0300

    debug bash rsa

--- generate4.py
+++ generate4.py
@@ -4,19 +4,10 @@ import subprocess as sp
 
 
 x = ""
-y = []
-
-def factors2(public):
-        i = (int(public))
-        xd = int(x)
-        if xd % i == 0:
-                y.append([xd, xd // i, i])
-
 with open(sys.argv[1]) as file:
     for line in file:
         x = line.rstrip()
         str2 = "factor " + str(x)
         output = sp.getoutput(str2)
-        y = output.split() 
-
-print("{}={}*{}".format(y[0][:-1],y[-1], int(y[0][:-1])//int(y[-1])))
+        y = output.split()
+        print("{}={}*{}".format(y[0][:-1], int(y[0][:-1])//int(y[1]), y[1]))
